e452fa805146e1812e3f3c8ed43aeaa26c4bf9c4
PaciStardust <git@paci.dev>
2026-01-10 01:27:33 +0100

    feat: :sparkles: sddm hardening

--- desktops/plasma/default.nix
+++ desktops/plasma/default.nix
@@ -4,6 +4,8 @@
 }:{
   # Enable the KDE Plasma Desktop Environment.
   services.displayManager.sddm.enable = true;
+  services.displayManager.sddm.wayland.enable = true;
+  services.displayManager.sddm.settings.General.DisplayServer = "wayland";
   services.desktopManager.plasma6.enable = true;
 
   environment.plasma6.excludePackages = with pkgs.kdePackages; [

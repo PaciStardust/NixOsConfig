d696a7ec91a714c9becd29f85f0325f4ecec412a
PaciStardust <git@paci.dev>
2026-01-14 11:10:39 +0100

    fix: :adhesive_bandage: disabled syncthing config to avoid resets

--- homeModules/services/syncthing.nix
+++ homeModules/services/syncthing.nix
@@ -6,19 +6,6 @@
   options.strdst-services.syncthing.enable = lib.mkEnableOption "STRDST Syncthing";
   
   config = lib.mkIf config.strdst-services.syncthing.enable {
-    services.syncthing = {
-      enable = true;
-      settings = {
-        gui = {
-          metricsWithoutAuth = false;
-        };
-        options = {
-          startBrowser = false;
-          urPostInsecurely = false;
-          minHomeDiskFree = "10%";
-          overwriteRemoteDeviceNamesOnConnect = true;
-        };
-      };
-    };
+    services.syncthing.enable = true;
   };
 }

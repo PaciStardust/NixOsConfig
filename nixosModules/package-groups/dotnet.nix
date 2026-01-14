b5b3c1a1057b1c6c02741c82f202e03102451666
PaciStardust <git@paci.dev>
2026-01-14 12:45:07 +0100

    feat: :bug: make dotnet actually usable

--- nixosModules/package-groups/dotnet.nix
+++ nixosModules/package-groups/dotnet.nix
@@ -3,20 +3,31 @@
   lib, 
   pkgs, 
   ...
-}:{
+}:
+let 
+  dotnet_combined = with pkgs.dotnetCorePackages; combinePackages[
+    sdk_10_0
+    sdk_9_0
+    sdk_8_0
+    runtime_10_0
+    runtime_9_0
+    runtime_8_0
+    aspnetcore_10_0
+    aspnetcore_9_0
+    aspnetcore_8_0
+  ];
+in{
   options.strdst-package-groups.dotnet.enable = lib.mkEnableOption "STRDST .NET Package Group";
   
   config = lib.mkIf config.strdst-package-groups.dotnet.enable {
-    environment.systemPackages = with pkgs; [
-      dotnet-aspnetcore_10
-      dotnet-aspnetcore_9
-      dotnet-aspnetcore_8
-      dotnet-sdk_10
-      dotnet-sdk_9
-      dotnet-sdk_8
-      dotnet-runtime_10
-      dotnet-runtime_9
-      dotnet-runtime_8
+    environment.systemPackages = [
+      dotnet_combined
     ];
+
+    environment.variables = {
+      DOTNET_ROOT = dotnet_combined;
+      DOTNET_CLI_TELEMETRY_OPTOUT = 1;
+      DOTNET_SKIP_FIRST_TIME_EXPERIENCE = 1;
+    };
   };
 }
